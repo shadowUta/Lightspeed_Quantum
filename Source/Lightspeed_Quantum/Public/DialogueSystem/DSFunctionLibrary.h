00cfdead281afcffb03f9065c2be83d7ad172253
shadowUta <182259078+shadowUta@users.noreply.github.com>
2026-02-18 13:46:54 +0800

    2.18Update - 修复最新文件无法编译问题

--- Source/Lightspeed_Quantum/Public/DialogueSystem/DSFunctionLibrary.h
+++ Source/Lightspeed_Quantum/Public/DialogueSystem/DSFunctionLibrary.h
@@ -5,3 +5,11 @@
 #include "NativeGameplayTags.h"
 #include "UObject/Class.h"
 #include "DSFunctionLibrary.generated.h"
+
+UCLASS()
+class LIGHTSPEED_QUANTUM_API UDSFunctionLibrary : public UBlueprintFunctionLibrary
+{
+
+    GENERATED_BODY()
+
+};
